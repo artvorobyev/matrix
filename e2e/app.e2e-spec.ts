3e634ef1ba7d4c45b1c0df37c392404cede0cc07
Artemy Vorobyev <79062272107@yandex.ru>
2020-11-26 20:49:50 +0300

    e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,111 @@ describe('matrix App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('Ввод размеров. Корректные размеры', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    const button = page.getElementById('startSubmitButton');
+    page.getElementById('firstWidth').sendKeys('2');
+    page.getElementById('firstHeight').sendKeys('3');
+    page.getElementById('secondWidth').sendKeys('4');
+    page.getElementById('secondHeight').sendKeys('2');
+    expect(button.getAttribute('disabled')).not.toBeTruthy();
+
+    button.click();
+    expect(page.getElementById('inputState')).toBeTruthy();
+  });
+
+  it('Ввод размеров. Ширина первой матрицы не равна высоте второй матрицы', () => {
+    page.navigateTo();
+    const button = page.getElementById('startSubmitButton');
+    page.getElementById('firstWidth').sendKeys('2');
+    page.getElementById('firstHeight').sendKeys('3');
+    page.getElementById('secondWidth').sendKeys('4');
+    page.getElementById('secondHeight').sendKeys('3');
+    expect(!!button.getAttribute('disabled')).toEqual(true);
+    expect(
+      page.getElementByText(
+        'Ширина первой матрицы должна быть равна высоте второй матрицы'
+      )
+    ).toBeTruthy();
+  });
+
+  it('Ввод размеров. Стороны матрицы < 1', () => {
+    page.navigateTo();
+    const button = page.getElementById('startSubmitButton');
+    page.getElementById('firstWidth').sendKeys('0');
+    page.getElementById('firstHeight').sendKeys('0');
+    page.getElementById('secondWidth').sendKeys('0');
+    page.getElementById('secondHeight').sendKeys('0');
+    expect(!!button.getAttribute('disabled')).toEqual(true);
+    expect(
+      page.getElementByText('Минимальное значение высоты и ширины = 1')
+    ).toBeTruthy();
+  });
+
+  it('Приложение должно обеспечивать ввод элементов матрицы, согласно введенным ранее ширине и высоте', () => {
+    page.navigateTo();
+    const button = page.getElementById('startSubmitButton');
+    page.getElementById('firstWidth').sendKeys('2');
+    page.getElementById('firstHeight').sendKeys('3');
+    page.getElementById('secondWidth').sendKeys('4');
+    page.getElementById('secondHeight').sendKeys('2');
+    button.click();
+    expect(page.getMatrixInputs('first').count()).toEqual(6);
+    expect(page.getMatrixInputs('second').count()).toEqual(8);
+  });
+
+  it('Приложение должно обеспечивать возврат к вводу размеров', () => {
+    page.navigateTo();
+    const button = page.getElementById('startSubmitButton');
+    page.getElementById('firstWidth').sendKeys('2');
+    page.getElementById('firstHeight').sendKeys('3');
+    page.getElementById('secondWidth').sendKeys('4');
+    page.getElementById('secondHeight').sendKeys('2');
+    button.click();
+    page.getElementByText('← Изменить размеры матриц').click();
+    expect(page.getElementById('startState')).toBeTruthy();
+  });
+
+  it('Приложение должно обеспечивать перемножение матрицы и вывод результата', () => {
+    page.navigateTo();
+    const button = page.getElementById('startSubmitButton');
+    page.getElementById('firstWidth').sendKeys('2');
+    page.getElementById('firstHeight').sendKeys('1');
+    page.getElementById('secondWidth').sendKeys('2');
+    page.getElementById('secondHeight').sendKeys('2');
+    button.click();
+    page.getElementById('first_1_1').sendKeys('1');
+    page.getElementById('first_1_2').sendKeys('2');
+    page.getElementById('second_1_1').sendKeys('3');
+    page.getElementById('second_1_2').sendKeys('4');
+    page.getElementById('second_2_1').sendKeys('5');
+    page.getElementById('second_2_2').sendKeys('6');
+    page.getElementById('inputSubmitButton').click();
+    expect(page.getElementsByCss('.matrix-element').count()).toEqual(2);
+    expect(page.getElementsByCss('.matrix-element').get(0).getText()).toEqual(
+      '13'
+    );
+    expect(page.getElementsByCss('.matrix-element').get(1).getText()).toEqual(
+      '16'
+    );
+  });
+
+  it('Приложение должно обеспечивать возврат к вводу элементов', () => {
+    page.navigateTo();
+    const button = page.getElementById('startSubmitButton');
+    page.getElementById('firstWidth').sendKeys('2');
+    page.getElementById('firstHeight').sendKeys('1');
+    page.getElementById('secondWidth').sendKeys('2');
+    page.getElementById('secondHeight').sendKeys('2');
+    button.click();
+    page.getElementById('first_1_1').sendKeys('1');
+    page.getElementById('first_1_2').sendKeys('2');
+    page.getElementById('second_1_1').sendKeys('3');
+    page.getElementById('second_1_2').sendKeys('4');
+    page.getElementById('second_2_1').sendKeys('5');
+    page.getElementById('second_2_2').sendKeys('6');
+    page.getElementById('inputSubmitButton').click();
+    page.getElementByText('← Изменить значения').click();
+    expect(page.getElementById('inputState')).toBeTruthy();
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -4,8 +4,19 @@ export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
+  getElementById(id: string) {
+    return element(by.id(id));
+  }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getElementByText(text: string) {
+    return element(by.cssContainingText('*', text));
+  }
+
+  getElementsByCss(selector: string) {
+    return element.all(by.css(selector));
+  }
+
+  getMatrixInputs(matrix: string) {
+    return element.all(by.css(`.matrix-form-${matrix} .value-input`));
   }
 }
