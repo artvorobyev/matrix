6e51df226b169723c40ad74d8ccc22e19907d41f
Artemy Vorobyev <79062272107@yandex.ru>
2020-11-26 12:03:58 +0300

    fix deploy

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,5 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-process.env.NO_PROXY = 'localhost, 0.0.0.0/4201, 0.0.0.0/9876';
-process.env.no_proxy = 'localhost, 0.0.0.0/4201, 0.0.0.0/9876';
 
 module.exports = function (config) {
   config.set({
@@ -35,7 +33,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['CHROMEWEBDRIVER'],
+    browsers: ['Chrome'],
     singleRun: false,
   });
 };
